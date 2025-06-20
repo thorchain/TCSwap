70ee70f609dcae001b58d279e2f3f6ba4f6e7175
towan <towan@swapkit.dev>
2025-06-20 10:49:47 +0400

    chore: fix wallets tests fixtures

--- packages/wallets/tests/fixtures.ts
+++ packages/wallets/tests/fixtures.ts
@@ -24,6 +24,7 @@ export const testKeystoreAddresses: Record<(typeof KEYSTORE_SUPPORTED_CHAINS)[nu
   [Chain.Solana]: "4UHWuPwyV3XCcnwrpEdWAEW3WLUP1RDMaK3ANNsRUjfM",
   [Chain.THORChain]: "thor1hm0sdz9v2h5jwjuu0ssp8x98upvlc6py2uq4vz",
   [Chain.Tron]: "THr473cZqHwBLEmTWUUR1WTotxijWWbRGD",
+  [Chain.Near]: "towanovic.near",
 };
 
 export const testKeystoreWalletData = {
